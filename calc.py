a26f881dcb0000f5da30d4e2a6ee29de14132b7e
Artem Borisov <150651635+DivotionProjects@users.noreply.github.com>
2024-09-10 10:43:04 +0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -7,5 +7,7 @@ def calc_sfsn(ref_path='ref.bmp', dis_path='dis.bmp'):
     imgref = np.double(cv2.cvtColor(cv2.imread(ref_path), cv2.COLOR_BGR2GRAY))
     imgdis = np.double(cv2.cvtColor(cv2.imread(dis_path), cv2.COLOR_BGR2GRAY))
 
+    sf, sn = compute_sf_sn(imgref, imgdis)
+
     pred_score = (1 + 0.9) * sf + (1 - 0.9) * sn
     return pred_score
